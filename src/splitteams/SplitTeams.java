375884b238f4b4000bfe9006952f16c39e0f720a
Diogolemoos <146637354+Diogolemoos@users.noreply.github.com>
2023-10-24 17:08:55 +0100

    Github link

--- src/splitteams/SplitTeams.java
+++ src/splitteams/SplitTeams.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+
+/* 
+Github link: https://github.com/Diogolemoos/SplitTeams
+*/
 package splitteams;
 
 import java.io.BufferedReader;
