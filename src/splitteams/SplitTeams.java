3585c0c70cf68c49fe8f84701de6cb37fa64e588
Diogolemoos <146637354+Diogolemoos@users.noreply.github.com>
2023-10-24 16:55:03 +0100

    Main code

--- src/splitteams/SplitTeams.java
+++ src/splitteams/SplitTeams.java
@@ -4,6 +4,12 @@
  */
 package splitteams;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author diogo
@@ -14,7 +20,62 @@ public class SplitTeams {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List<Person> allPeople = readDataFromFile("MOCK_DATA.csv");
+        List<Team> teams = generateTeams(allPeople, 20, 5);
+
+        for (Team team : teams) {
+            System.out.println("Team: " + team.getName());
+            for (Person member : team.getMembers()) {
+                System.out.println(member.getId()+ " - " + member.getFirstName() + " " + member.getLastName () + " - email: " + member.getEmail());
+            }
+            System.out.println();
+        }
+    }
+
+    private static List<Person> readDataFromFile(String filename) {
+        List<Person> people = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 4) {
+                    int id = Integer.parseInt(parts[0]);
+                    String firstName = parts[1];
+                    String lastName = parts[2];
+                    String email = parts[3];
+                    people.add(new Person(id, firstName, lastName, email));
+                }
+            }
+        } catch (Exception e) {
+            
+        }
+        return people;
+    }
+
+    private static List<Team> generateTeams(List<Person> people, int numTeams, int teamSize) {
+        if (people.size() < numTeams * teamSize) {
+            System.out.println("No more people to create the teams.");
+            return Collections.emptyList();
+        }
+
+        Collections.shuffle(people);
+        List<Team> teams = new ArrayList<>();
+        List<Person> availablePeople = new ArrayList<>(people);
+
+        for (int i = 1; i <= numTeams; i++) {
+            Collections.shuffle(availablePeople);
+            List<Person> teamMembers = new ArrayList<>(teamSize);
+
+            for (int j = 0; j < teamSize; j++) {
+                teamMembers.add(availablePeople.remove(0));
+            }
+
+            String teamName = "Team " + i;
+            teams.add(new Team(teamName, teamMembers));
+        }
+
+        return teams;
+    }
     }
     
-}
+
--- src/splitteams/Team.java
+++ src/splitteams/Team.java
@@ -4,10 +4,26 @@
  */
 package splitteams;
 
+import java.util.List;
+
 /**
  *
  * @author diogo
  */
 public class Team {
-    
+    private String name;
+    private List<Person> members;
+
+    public Team(String name, List<Person> members) {
+        this.name = name;
+        this.members = members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
 }
